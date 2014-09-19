bf96b583645214f915d1caf787b7e1dc1a17b7a1
masterdagon <Muggi@10.50.138.183>
2014-09-19 15:18:00 +0200

    Added blabla method

---
+++ src/sep2014_excercise5/Sep2014_Excercise5.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package sep2014_excercise5;
+
+/**
+ *
+ * @author Muggi
+ */
+public class Sep2014_Excercise5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
