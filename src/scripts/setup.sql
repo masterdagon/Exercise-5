a278ada81ea804e17e1caf4fdfdb6f7cd72f407a
masterdagon <Muggi@10.50.138.183>
2014-09-19 15:29:44 +0200

    Added setup.sql in the script package, with scripts to create person table in database

--- src/scripts/setup.sql
+++ src/scripts/setup.sql
@@ -9,5 +9,3 @@ insert into Person values(100,'Peter','Hansen');
 insert into Person values(110,'Lone','Hansen');
 insert into Person values(120,'John','McDonald');
 insert into Person values(130,'Peter','Jensen');
-
-//ghdhd
